69a4b3cde5f79af9a620c20dc2cc8195f987e72b
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-11-18 06:02:43 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/update'
+  get 'users/show'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
 end
